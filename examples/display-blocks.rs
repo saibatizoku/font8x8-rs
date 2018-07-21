8d716913a467b001627bcb74628074422a33897d
Joaquín Rosales <globojorro@gmail.com>
2018-07-20 19:17:23 -0500

    add an example showing how to print using UnicodeFonts::print_set

---
+++ examples/display-blocks.rs
@@ -0,0 +1,9 @@
+extern crate font8x8;
+
+use font8x8::*; //{BLOCK_FONTS, UnicodeFonts};
+
+fn main() {
+    // Use the convenience `print_set` method to view all the 
+    // Block Fonts on your screen.
+    BLOCK_FONTS.print_set();
+}
