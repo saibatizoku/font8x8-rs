0cae67092c895bbdfb971f667380647e85290a6f
Joaquín Rosales <globojorro@gmail.com>
2018-07-22 16:14:36 -0500

    modify examples/legacy.rs to run only with default features

--- examples/legacy.rs
+++ examples/legacy.rs
@@ -1,9 +1,11 @@
-#![feature(default)]
+#[cfg(feature = "default")]
 extern crate font8x8;
 
+#[cfg(feature = "default")]
 use font8x8::legacy::GREEK_LEGACY;
 
 // This example will print to the screen the first item on the GREEK_LEGACY array.
+#[cfg(feature = "default")]
 fn main() {
     for x in &GREEK_LEGACY[0] {
         for bit in 0..8 {
@@ -14,4 +16,8 @@ fn main() {
         }
         println!()
     }
+}
+
+#[cfg(not(feature = "default"))]
+fn main() {
 }
